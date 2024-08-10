68bf3dfdf985357b8fbeb8409f47d3a96fdfaeba
Shivani Jamwal <s.shivani.1@research.gla.ac.uk>
2024-08-10 23:49:38 +0100

    update 1008 11:49

--- keygen_server/src/lib.rs
+++ keygen_server/src/lib.rs
@@ -1,31 +1,41 @@
-use wasm_bindgen::prelude::*;
-use rabe::schemes::bsw::{self, CpAbePublicKey, CpAbeCiphertext};
-use rabe::utils::policy::pest::PolicyLanguage::JsonPolicy;
+extern crate rabe;
+use crate::rabe::schemes::*;
+use rabe::schemes::bsw::{CpAbeCiphertext, CpAbePublicKey, CpAbeSecretKey, CpAbeMasterKey};
+use rabe::utils::policy::pest::PolicyLanguage;
 use serde_json;
+use wasm_bindgen::prelude::*;
+use js_sys::Uint8Array;
 
 #[wasm_bindgen]
-pub fn generate_key() -> String {
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+#[wasm_bindgen]
+pub fn setup() -> Result<JsValue, JsValue> {
     let (pk, msk) = bsw::setup();
-    format!("PK: {:?}, MSK: {:?}", pk, msk)
+    let pk_json = serde_json::to_string(&pk).unwrap_or_else(|_| "Failed to serialize public key".to_string());
+    let msk_json = serde_json::to_string(&msk).unwrap_or_else(|_| "Failed to serialize master secret key".to_string());
+
+    let result = (pk_json, msk_json);
+
+    serde_wasm_bindgen::to_value(&result)
+        .map_err(|e| e.into())
 }
 
 #[wasm_bindgen]
-pub fn encrypt_data(public_key_json: &str, policy: &str, plaintext: &str) -> String {
+pub fn encrypt(pk_json: &str, policy: &str, plaintext: Vec<u8>) -> Result<String, JsValue> {
+    let pk: CpAbePublicKey = serde_json::from_str(pk_json)
+        .map_err(|e| JsValue::from_str(&e.to_string()))?;
 
-    let public_key: CpAbePublicKey = serde_json::from_str(public_key_json)
-        .expect("Failed to deserialize public key");
+    let policy_string = policy.to_string();
     
-    // let policy_value = parse(policy, JsonPolicy).unwrap();
-
-    let ciphertext = bsw::encrypt(
-        &public_key, 
-        policy, 
-        JsonPolicy, //JSON we go with this assumption --
-        plaintext.as_bytes(),
-    ).expect("Encryption failed");
-
-    let serialized_ciphertext = serde_json::to_string(&ciphertext).expect("Serialization failed");
-    let encrypted_str = base64::encode(&serialized_ciphertext);
-
-    encrypted_str
+    let ct_cp: CpAbeCiphertext = bsw::encrypt(&pk, &policy_string, PolicyLanguage::HumanPolicy, &plaintext)
+        .map_err(|e| JsValue::from_str(&e.to_string()))?;
+    
+    serde_json::to_string(&ct_cp)
+        .map_err(|e| JsValue::from_str(&e.to_string()))
 }
+
+
+
